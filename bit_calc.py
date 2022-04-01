1b2905c1b6eb85e983a5b644e8e5df9ca89ce662
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-04-02 11:53:47 +1300

    Add files via upload
    fixes

--- bit_calc.py
+++ bit_calc.py
@@ -53,6 +53,34 @@ def user_choice():
             print("Please choose a valid file type!")
             print()
 
+# checks input is a number more than a given value
+def num_check(question, low):
+    valid = False
+    while not valid:
+        
+        error = "Please enter an integer that is more than"
+        "(or equal to)". format(low)
+            
+        try:
+            
+            # asks the user to enter a number
+            response = int(input(question))
+            
+            # checks number  is more than zero
+            if response >= low:
+                return response
+                
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+            
+        except ValueError:
+            print(error)
+            
+            
+            
+
 # Main routine goes here
 
 # Heading
@@ -67,9 +95,19 @@ while keep_going == "":
     # ask the user for the file type
     data_type = user_choice()   
     print("You chose", data_type)      
-    
+
     # For integers, ask for integer
+    if data_type =="integer":
+        var_integer = num_check("Enter a integer: ", 0)
+    
+    # For images, ask for width and height    
     # (must be an integer more than / equal to 0)
-    
-    # for images, ask for width and height
-    
+    elif  data_type == "image":
+        image_width = num_check("image width? ", 1)
+        print()
+        image_height = num_check("image height? ", 1)
+
+    # for text, ask for a string
+    else:
+        var_text = input("enter some text: ")
+        
